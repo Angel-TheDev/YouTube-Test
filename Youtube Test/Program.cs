4be7839b22a70f6ffb3f8e6b8fad992009072dad
Angel-TheDev <angelantoinetted@yahoo.com>
2019-05-18 12:42:22 -0400

    first commit

---
+++ Youtube Test/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Youtube_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "John";
+            int characterAge = 32;
+            string characterName2 = "Mike";
+
+            Console.WriteLine(characterName + " and " + characterName2 + " loved to go biking together");
+            Console.WriteLine(characterName + " and " + characterName2 + " are both " + characterAge + ", and both are from Canada");
+           
+            // Note* If we wanted to change the Value halfway through the story we could just modify the Value.
+
+            characterName = "Johnny";
+
+            Console.WriteLine("John really liked his nickname " + characterName + " though, so for now on we'll call him " + characterName);
+
+            Console.ReadLine();
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
